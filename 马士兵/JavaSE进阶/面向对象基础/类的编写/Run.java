4b718efe9fa0f1d88f6da55d0280abc53f86c073
xiaorui3 <3505469466@qq.com>
2025-07-03 19:56:23 +0800

    2025 7 3 新开端

---
+++ 马士兵/JavaSE进阶/面向对象基础/类的编写/Run.java
@@ -0,0 +1,9 @@
+package 马士兵.JavaSE进阶.面向对象基础.类的编写;
+
+public class Run {
+    public static void main(String[] args) {
+        Person p1=new Person();
+        p1.setName("张三");
+        p1.SpeakName();
+    }
+}
