9f66a108487ba78cba4948aa68f127809cffc511
xiaorui3 <3505469466@qq.com>
2025-07-10 16:49:46 +0800

    布卡

--- 布卡/基础/Demo.java
+++ 布卡/基础/Demo.java
@@ -3,6 +3,11 @@ package 布卡.基础;
 public class Demo {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        int a=1;
+        int b=2;
+        int sum=a++ + ++b - a-- - --b +a-- - --b;
+
+        System.out.println(sum);
     }
 }
 
