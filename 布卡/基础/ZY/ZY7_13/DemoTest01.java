52e470da98dc08a04fb71086010c1272e3ca1dc3
xiaorui3 <3505469466@qq.com>
2025-07-13 11:31:15 +0800

    布卡 ROG

---
+++ 布卡/基础/ZY/ZY7_13/DemoTest01.java
@@ -0,0 +1,8 @@
+package 布卡.基础.ZY.ZY7_13;
+
+public class DemoTest01 {
+    public void arrayTest(int[] arr)
+    {
+        arr[0]=4563440;
+    }
+}
--- 布卡/基础/ZY/ZY7_13/Test.java
+++
@@ -1,4 +0,0 @@
-package 布卡.基础.ZY.ZY7_13;
-
-public class Test {
-}
